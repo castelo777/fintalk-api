b0a7a476f1002a9b1309bf76071685e30c12ffe4
José Castelo Neto <josecastelo02@gmail.com>
2019-09-18 23:32:46 -0300

    port deploy

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require('dotenv').config({ path: './.env' })
 const http = require("http")
 const app = require("./config");
 
-http.createServer(app).listen(process.env.APP_PORT, () => {
+http.createServer(app).listen(process.env.PORT || process.env.APP_PORT, () => {
     console.log(`_______________ SERVER START ON ${process.env.APP_PORT} _______________`)
 })
